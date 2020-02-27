21cec2c709edfb0bbd4ee71267043e16290ebda3
Langston Nashold <lgnashold@gmail.com>
2020-02-27 01:20:08 -0800

    Tried to add deadline

--- examples/blink/main.c
+++ examples/blink/main.c
@@ -1,9 +1,19 @@
 #include <led.h>
 #include <timer.h>
+#include <console.h>
+char hello1[] = "ENtry World!\r\n";
 
+static void nop(
+  int a __attribute__((unused)),
+  int b __attribute__((unused)),
+  int c __attribute__((unused)),
+  void* d __attribute__((unused))) {}
 int main(void) {
   // Ask the kernel how many LEDs are on this board.
   int num_leds = led_count();
+
+
+  putnstr_async(hello1, sizeof(hello1), nop, NULL);
 
   // Blink the LEDs in a binary count pattern and scale
   // to the number of LEDs on the board.
